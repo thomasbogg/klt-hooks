3dfe665ab556465c7691ebd03d8ebfc1747b4711
thomasbogg <thomasbogg@gmail.com>
2026-05-07 20:15:59 +0100

    Reconfigured server settings for successful web deployment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
@@ -6,3 +7,8 @@ app = Flask(__name__)
 def hook():
     print(request.data)
     return "Hello, world!"
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
