a4b15c262aa7f7de7eb09016a37bc56cad83f834
thomasbogg <thomasbogg@gmail.com>
2026-05-07 22:53:54 +0100

    Added gunicorn for production running

--- request.py
+++ request.py
@@ -1,6 +1,12 @@
 import requests
+import json
+from settings import SECRET_KEY
 
 url = 'https://klt-hooks.up.railway.app/webhook'
 
-r = requests.post(url, data='This is the data')
+headers = {
+    "Authorization": f"Bearer {SECRET_KEY}"
+}
+
+r = requests.post(url, data=json.dumps({"message": "This is the data"}), headers=headers)
 print(r.content)
