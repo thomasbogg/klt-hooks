285bc578a8d000197f861c00d15f276007ebe4e4
thomasbogg <thomasbogg@gmail.com>
2026-05-07 21:58:50 +0100

    Added Waitress module to script for deployment service

--- main.py
+++ main.py
@@ -10,5 +10,6 @@ def hook():
 
 
 if __name__ == "__main__":
+    from waitress import serve
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    serve(app, host="0.0.0.0", port=port)
