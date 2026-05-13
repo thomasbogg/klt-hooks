11e806182741a3ea47f5a8831d0c39cd58e6d962
thomasbogg <thomasbogg@gmail.com>
2026-05-13 15:36:58 +0100

    Started building Revolut API integration

--- main.py
+++ main.py
@@ -19,6 +19,13 @@ def wisecallback():
     return "Wise Money In contact received!"
 
 
+@app.route("/revolutcallback", methods=["POST"])
+def revolutcallback():
+    data = json.loads(request.data)  # Attempt to parse the incoming data as JSON
+    print(f"Received data: {data}")
+    return "Revolut contact received!"
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
