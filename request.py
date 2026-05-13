bf94c352d9577aae3d9022125268d1403508f62a
thomasbogg <thomasbogg@gmail.com>
2026-05-13 15:39:35 +0100

    Started building Revolut API integration

--- request.py
+++ request.py
@@ -1,12 +1,34 @@
+#import requests
+#import json
+#from settings import SECRET_KEY
+#
+#url = 'https://klt-hooks.up.railway.app/webhook'
+#
+#headers = {
+#    "Authorization": f"Bearer {SECRET_KEY}"
+#}
+#
+#r = requests.post(url, data=json.dumps({"message": "This is the data"}), headers=headers)
+#print(r.content)
+#
 import requests
 import json
-from settings import SECRET_KEY
 
-url = 'https://klt-hooks.up.railway.app/webhook'
+url = "https://sandbox-merchant.revolut.com/api/webhooks"
 
+payload = json.dumps({
+  "url": "https://example.com/webhooks",
+  "events": [
+    "ORDER_COMPLETED",
+    "ORDER_AUTHORISED"
+  ]
+})
 headers = {
-    "Authorization": f"Bearer {SECRET_KEY}"
+  'Content-Type': 'application/json',
+  'Accept': 'application/json',
+  'Authorization': 'Bearer <yourSecretApiKey>'
 }
 
-r = requests.post(url, data=json.dumps({"message": "This is the data"}), headers=headers)
-print(r.content)
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
