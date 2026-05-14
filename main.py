5dd2e44d0eca4b118c286d5ace1db5c47879faf4
thomasbogg <thomasbogg@gmail.com>
2026-05-14 16:54:22 +0100

    Added email to self for Revolut testing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from correspondence.self.functions import new_email_to_self, send_email_to_self
 from flask import Flask, request
-from settings import LOCAL, KLT_SECRET_KEY
 import json
 import os
 
@@ -23,9 +23,15 @@ def wisecallback():
 def revolutcallback():
     data = json.loads(request.data)  # Attempt to parse the incoming data as JSON
     print(f"Received data: {data}")
+    user, message = new_email_to_self(subject = "Revolut contact received")
+    message.body.paragraph(f"Received data: {data}")
+    send_email_to_self(user, message)
     return "Revolut contact received!"
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
+    user, message = new_email_to_self(subject = "KLT Hooks started")
+    message.body.paragraph("KLT Hooks has started successfully.")
+    send_email_to_self(user, message)
